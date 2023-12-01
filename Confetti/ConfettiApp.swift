2692c37d38e7d22b14798666c40c30fe4eec0bae
Paul Schwind <git@pschwind.de>
2023-12-01 17:43:42 +0100

    Initial Commit

---
+++ Confetti/ConfettiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConfettiApp.swift
+//  Confetti
+//
+//  Created by Paul Schwind on 01.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ConfettiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
