7b4233ee4c1db2acc850ca88cd020a5ab2f54210
Paul Schwind <git@pschwind.de>
2023-12-04 02:48:50 +0100

    Add option to launch at startup

--- Confetti/SettingsScreen.swift
+++ Confetti/SettingsScreen.swift
@@ -7,13 +7,16 @@
 
 import SwiftUI
 import KeyboardShortcuts
+import LaunchAtLogin
 
 struct SettingsScreen: View {
     var body: some View {
         Form {
             KeyboardShortcuts.Recorder("Show Confetti:", name: .showConfetti)
             KeyboardShortcuts.Recorder("Toggle mouse confetti:", name: .toggleMouseConfettiCannonEnabled)
+            LaunchAtLogin.Toggle()
         }
-        .padding(.all)
+        .frame(minWidth: 400)
+        .padding(.all, 24)
     }
 }
